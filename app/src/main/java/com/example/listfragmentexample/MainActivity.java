c1ee7e83857a433d2b41d4e99f6d9554411de02d
Amaury Esparza <amauryesparza@gmail.com>
2014-09-04 18:13:02 -0500

    Pattern Design solved

--- app/src/main/java/com/example/listfragmentexample/MainActivity.java
+++ app/src/main/java/com/example/listfragmentexample/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.listfragmentexample;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends Activity implements MyListFragment.CommunicatorInterface{
 
-    MyListFragment listFragment;
     FragmentDescription fragmentDescription;
     FragmentManager fragmentManager;
 
@@ -19,13 +19,12 @@ public class MainActivity extends Activity implements MyListFragment.Communicato
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fragmentManager = getFragmentManager();
-        listFragment = new MyListFragment();
-        listFragment.setCommunicator(this);
     }
 
     @Override
     public void respond(int index){
-        fragmentDescription = (FragmentDescription) fragmentManager.findFragmentById(R.id.description_fragment);
+        Log.d("MainActivity", "respond");
+        fragmentDescription = (FragmentDescription) fragmentManager.findFragmentById(R.id.fragment2);
         fragmentDescription.changeData(index);
 
 
--- app/src/main/java/com/example/listfragmentexample/MyListFragment.java
+++ app/src/main/java/com/example/listfragmentexample/MyListFragment.java
@@ -1,6 +1,8 @@
 package com.example.listfragmentexample;
+import android.app.Activity;
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,36 +15,47 @@ import android.widget.Toast;
  * Created by Amaury Esparza on 30/08/2014.
  */
 
-public class MyListFragment extends ListFragment implements AdapterView.OnItemClickListener{
+public class MyListFragment extends ListFragment{
 
     CommunicatorInterface communicator;
 
-    public void setCommunicator(CommunicatorInterface communicator){
-        this.communicator = communicator;
+    public interface CommunicatorInterface{
+        public void respond(int index);
     }
 
     @Override
+    public void onAttach(Activity activity){
+        Log.d("MyListFragment", "onAttach");
+        super.onAttach(activity);
+        try{
+            communicator = (CommunicatorInterface) activity;
+        }catch(ClassCastException ex){
+            throw new ClassCastException(activity.toString() + " must implement CommunicatorInterface");
+        }
+    }
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        Log.d("MyListFragment", "onCreateView");
         return inflater.inflate(R.layout.list_fragment, container, false);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
+        Log.d("MyListFragment", "onActivityCreated");
         super.onActivityCreated(savedInstanceState);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.star_wars_characters,android.R.layout.simple_list_item_1);
         setListAdapter(adapter);
-        getListView().setOnItemClickListener(this);
     }
 
     @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
-        communicator.respond(i);
-        Toast.makeText(getActivity(), "Item "+i, Toast.LENGTH_LONG).show();
-
+    public void onListItemClick(ListView l, View v, int position, long id){
+        Log.d("MyListFragment", "onListItemClick");
+        communicator.respond(position);
+        Toast.makeText(getActivity(), "Item " + position , Toast.LENGTH_LONG).show();
     }
 
-    public interface CommunicatorInterface{
-        public void respond(int index);
-    }
+
+
+
 
 }
